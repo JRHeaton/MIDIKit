1f6b72890dfb842051dce651ef9da452987e6e89
John Heaton <jheaton.dev@gmail.com>
2014-03-23 00:12:20 -0400

    Initial Commit

---
+++ MIDIKit/MIDIKit.h
@@ -0,0 +1,13 @@
+//
+//  MIDIKit.h
+//  MIDIKit
+//
+//  Created by John Heaton on 3/23/14.
+//  Copyright (c) 2014 John Heaton. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MIDIKit : NSObject
+
+@end
---
+++ MIDIKit/MIDIKit.m
@@ -0,0 +1,13 @@
+//
+//  MIDIKit.m
+//  MIDIKit
+//
+//  Created by John Heaton on 3/23/14.
+//  Copyright (c) 2014 John Heaton. All rights reserved.
+//
+
+#import "MIDIKit.h"
+
+@implementation MIDIKit
+
+@end
