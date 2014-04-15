d8805ac46630706d50038ed816a291c00966d87a
John Heaton <jheaton.dev@gmail.com>
2014-04-15 08:09:09 -0400

    removed more fugly pragmas

--- MIDIKit/MKClient.h
+++ MIDIKit/MKClient.h
@@ -15,8 +15,6 @@
 @protocol MKClientNotificationDelegate;
 
 
-#pragma mark - -Mutual ObjC/JavaScript-
-
 /*
  MKClient is the client class to the system MIDI server.
  It 'owns' or manages any created ports or endpoints,
@@ -25,14 +23,9 @@
 
 @protocol MKClientJS <JSExport, MKObjectJS>
 
-#pragma mark - -Init-
 + (instancetype)global;
 + (instancetype)clientWithName:(NSString *)name;
 
-
-#pragma mark - -Child Objects-
-
-#pragma mark Ports
 // firstInputPort and firstOutputPort will look to see if there are
 // any already-created ports in the arrays below, and if not, create one,
 // and return it.
@@ -48,25 +41,17 @@
 - (MKInputPort *)createInputPortNamed:(NSString *)name;
 - (MKOutputPort *)createOutputPortNamed:(NSString *)name;
 
-#pragma mark Endpoints
 - (MKVirtualSource *)createVirtualSource;
-- (MKVirtualDestination *)createVirtualDestination;
-
 - (MKVirtualSource *)createVirtualSourceNamed:(NSString *)name;
+- (MKVirtualDestination *)createVirtualDestination;
 - (MKVirtualDestination *)createVirtualDestinationNamed:(NSString *)name;
 
-
-#pragma mark - -CoreMIDI Client Disposal-
 // Disposes the MIDIRef(MIDIClientRef) object (invalidates this object)
 - (instancetype)dispose;
 
-
-#pragma mark - -Notifications-
 + (void)startSendingNotifications;
 + (void)stopSendingNotifications;
 
-
-#pragma mark - -Child Object Containers-
 // If the convenience methods for instantiation of ports and endpoints
 // on this class is used, they are inserted automatically into these
 // containers. If not, you may manually insert your own.
@@ -78,47 +63,32 @@
 @end
 
 
-#pragma mark - -Client Wrapper-
 @interface MKClient : MKObject <MKClientJS, MKClientProperties>
 
 
-#pragma mark - -Global Notificadtion Delegates-
 - (instancetype)addNotificationDelegate:(id<MKClientNotificationDelegate>)delegate;
 - (instancetype)removeNotificationDelegate:(id<MKClientNotificationDelegate>)delegate;
 
 @end
 
 
-#pragma mark - -Global Notification Delegate-
 @protocol MKClientNotificationDelegate <NSObject>
 
 @optional
-#pragma mark - -Object Addition/Removal-
-
-#pragma mark All Objects
 // Called when anything is added/removed
 - (void)midiClient:(MKClient *)client objectAdded:(MKObject *)object ofType:(MIDIObjectType)type;
 - (void)midiClient:(MKClient *)client objectRemoved:(MKObject *)object ofType:(MIDIObjectType)type;
 
-#pragma mark Devices
 // Called for add/remove of specific types
 - (void)midiClient:(MKClient *)client deviceAdded:(MKDevice *)device;
 - (void)midiClient:(MKClient *)client deviceRemoved:(MKDevice *)device;
-
-#pragma mark Entities
 - (void)midiClient:(MKClient *)client entityAdded:(MKEntity *)entity;
 - (void)midiClient:(MKClient *)client entityRemoved:(MKEntity *)entity;
-
-#pragma mark Sources
 - (void)midiClient:(MKClient *)client sourceAdded:(MKSource *)source;
 - (void)midiClient:(MKClient *)client sourceRemoved:(MKSource *)source;
-
-#pragma mark Destinations
 - (void)midiClient:(MKClient *)client destinationAdded:(MKDestination *)destination;
 - (void)midiClient:(MKClient *)client destinationRemoved:(MKDestination *)destination;
 
-
-#pragma mark - -Updates & Errors-
 - (void)midiClient:(MKClient *)client object:(MKObject *)object changedValueOfPropertyForKey:(CFStringRef)key;
 
 // errorCode is a wrapper of an OSStatus
@@ -129,18 +99,13 @@
 @end
 
 
-#pragma mark - -Notifications-
-
-#pragma mark Names
+// Notifications
 extern NSString *MKObjectPropertyChangedNotification;
-
-#pragma mark UserInfo Keys
+// User info keys for notifications
 extern NSString *MKUserInfoPropertyNameKey;
 extern NSString *MKUserInfoObjectInstanceKey;
 
 
-#pragma mark - -Client Reference Protocol-
-// ---------------------------------------------------
 // Protocols used for instaniation/reference to client
 @protocol MKClientReference <JSExport>
 
@@ -149,7 +114,6 @@ extern NSString *MKUserInfoObjectInstanceKey;
 @end
 
 
-#pragma mark - -Client Instantiation Protocol-
 // Common protocol for objects that are created with a parent client
 // and a name.
 @protocol MKClientDependentInstaniation <NSObject, MKClientReference>
