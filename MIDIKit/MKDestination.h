3dbcc59edd557d4dc41bdc9bf9b367a0d0527831
John Heaton <jheaton.dev@gmail.com>
2014-04-16 19:10:56 -0400

    removed old extraneous getters from headers

--- MIDIKit/MKDestination.h
+++ MIDIKit/MKDestination.h
@@ -11,21 +11,6 @@
 @class MKEntity, MKOutputPort, MKMessage;
 @protocol MKDestinationJS <JSExport, MKObjectJS, MKEnumerableObjectJS>
 
-+ (NSUInteger)numberOfDestinations;
-+ (NSUInteger)count; // shorthand
-+ (NSArray *)all;
-+ (NSArray *)allOnline;
-+ (NSArray *)allOffline;
-
-JSExportAs(atIndex,                 + (instancetype)destinationAtIndex:(NSUInteger)index);
-
-JSExportAs(firstNamed,              + (instancetype)firstDestinationNamed:(NSString *)name);
-JSExportAs(firstContaining,         + (instancetype)firstDestinationContaining:(NSString *)namePart);
-JSExportAs(firstOnlineNamed,        + (instancetype)firstOnlineDestinationNamed:(NSString *)name);
-JSExportAs(firstOnlineContaining,   + (instancetype)firstOnlineDestinationContaining:(NSString *)namePart);
-JSExportAs(firstOfflineNamed,       + (instancetype)firstOfflineDestinationNamed:(NSString *)name);
-JSExportAs(firstOfflineContaining,  + (instancetype)firstOfflineDestinationContaining:(NSString *)namePart);
-
 JSExportAs(sendMessage,     - (instancetype)sendMessage:(MKMessage *)msg usingOutputPort:(MKOutputPort *)outputPort);
 JSExportAs(sendMessages,    - (instancetype)sendMessages:(NSArray *)messages usingOutputPort:(MKOutputPort *)outputPort);
 
--- MIDIKit/MKSource.h
+++ MIDIKit/MKSource.h
@@ -11,21 +11,6 @@
 @class MKEntity;
 @protocol MKSourceJS <JSExport, MKObjectJS, MKEnumerableObjectJS>
 
-+ (NSUInteger)numberOfSources;
-+ (NSUInteger)count;
-+ (NSArray *)all;
-+ (NSArray *)allOnline;
-+ (NSArray *)allOffline;
-
-JSExportAs(atIndex,                 + (instancetype)sourceAtIndex:(NSUInteger)index);
-
-JSExportAs(firstNamed,              + (instancetype)firstSourceNamed:(NSString *)name);
-JSExportAs(firstContaining,         + (instancetype)firstSourceContaining:(NSString *)namePart);
-JSExportAs(firstOnlineNamed,        + (instancetype)firstOnlineSourceNamed:(NSString *)name);
-JSExportAs(firstOnlineContaining,   + (instancetype)firstOnlineSourceContaining:(NSString *)namePart);
-JSExportAs(firstOfflineNamed,       + (instancetype)firstOfflineSourceNamed:(NSString *)name);
-JSExportAs(firstOfflineContaining,  + (instancetype)firstOfflineSourceContaining:(NSString *)namePart);
-
 @property (nonatomic, readonly) MKEntity *entity;
 
 @end
