4a91edc87fb45e5868d46ba221243256db5c3d05
John Heaton <jheaton.dev@gmail.com>
2016-06-04 23:13:10 -0400

    Host time calculations for MIDITimeStamp

--- MIDIKit/Extensions/MIDITimeStamp.swift
+++ MIDIKit/Extensions/MIDITimeStamp.swift
@@ -1,5 +1,14 @@
 import CoreMIDI
+import CoreAudio
 
 extension MIDITimeStamp {
 	public static var Now: MIDITimeStamp = 0
+	
+	public var secondsAgo: NSTimeInterval {
+		return NSTimeInterval(AudioConvertHostTimeToNanos(AudioGetCurrentHostTime() - self)) / 1_000_000_000
+	}
+	
+	public var date: NSDate {
+		return NSDate().dateByAddingTimeInterval(-secondsAgo)
+	}
 }
