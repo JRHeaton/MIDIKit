1f89c4e11b6c492a775a68761e820666657e5c84
John Heaton <jheaton.dev@gmail.com>
2016-06-04 23:14:05 -0400

    Updates to Launchpad playground

--- Experiments.playground/Contents.swift
+++ Experiments.playground/Contents.swift
@@ -2,34 +2,41 @@ import MIDIKit
 import Launchpad
 
 do {
-	let connection = try Connection(client: Client()) {
-		dump($0)
+	let client = try Client()
+	let outputPort = try client.createOutputPort()
+	let inputPort = try client.createInputPort { packet, source in
+		print("\(source) sent \(packet) ")
 	}
 	
 	if let dest = Destination.allLazy
 		.filter({ $0[.Name] == "Launchpad S" })
 		.first {
-		connection.destinations.append(dest)
 		
-		let send = { (cmd: Launchpad.Command) in try connection.send(cmd, onChannel: 0) }
+		print("sending light commands...")
+		try outputPort.send(
+			Command.fillGrid { row, column in
+				func rando(max: Brightness = .Full) -> Brightness {
+					return arc4random() % 2 == 0 ? .Off : max
+				}
+				return (rando(), rando())
+			},
+			onChannel: 0,
+			toDestination: dest)
 		
-		let cmds = Command.fillGrid { row, column in
-			func rando(max: Brightness = .Full) -> Brightness {
-				return arc4random() % 2 == 0 ? .Off : max
-			}
-			return (rando(), rando(.Low))
-		}
-		try connection.outputPort.send(cmds, onChannel: 0, toDestination: dest)
-		try connection.outputPort.send([0xb0, 0x69, 127], toDestination: dest)
-//		try send(.Reset)
+		//		try send(.Reset)
+	} else {
+		print("Launchpad destination not found")
 	}
 	
 	if let src = Source.allLazy
 		.filter({ $0[.Name] == "Launchpad S" })
 		.first {
-		connection.inputPort.connectSource(src)
+		try inputPort.connectSource(src)
+		print("source connected. listening...")
 		
 		CFRunLoopRun()
+	} else {
+		print("Launchpad source not found")
 	}
 	
 } catch let error {
